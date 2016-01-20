7bc6d32603baf8cc5b9f7105426e128f109248b9
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-01-20 13:35:11 +0100

    Finalize get_next_line

--- corrector.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-make -C libft fclean
-make -C libft
-gcc -Wall -Wextra -Werror -I libft/includes/ -c get_next_line.c -g
-gcc -Wall -Wextra -Werror -I libft/includes/ -c main.c -g
-gcc -o test_gnl get_next_line.o main.o -L libft/ -lft -g
